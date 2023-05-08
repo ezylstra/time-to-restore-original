015960ecf15b5878366f84f555f8da6b753f91c6
alyssarosemartin <alyssarosemartin@gmail.com>
2023-05-08 17:38:46 -0400

    code and file updates
    Populated climate variables from daymet for 2022 (since not yet available from NPN), using daymetr and the new script here - daymet_download.R
    Updated cues_flower.R to get and integrate that daymet data with the observational data.
    The data file is now all 8 Time to Restore priority species.
    You can run the models by starting on line 70 of cues_flower.R, and loading up all the species data (8priorityspp_2009-2022.csv)

--- cues_flower.R
+++ cues_flower.R
@@ -4,10 +4,10 @@ library(lubridate)
 library(ggplot2)
 library(ggpmisc)
 library(ggpubr)
+library(ggforce)
 library(lme4)
 library(mblm)
 rm(list=ls())
-
 setwd("~/Documents/My Files/USA-NPN/Data/Analysis/R_default/npn_analyses/TimetoRestore/Data")
 
 #This script performs preliminary analyses on the cues for blooming in pollinator resource
@@ -17,91 +17,92 @@ setwd("~/Documents/My Files/USA-NPN/Data/Analysis/R_default/npn_analyses/TimetoR
 species<-npn_species()
 
 #download individual phenometric data, with supporting fields, including Daymet climate data (only thru 2021 currently - 2022 coming soon)
-df <- npn_download_individual_phenometrics(
+#just 2022
+df2022 <- npn_download_individual_phenometrics(
   request_source = 'Alyssa', 
-  years = c(2009:2023), 
-  species_ids = c(202), 
+  years = c(2022), 
+  species_ids = c(931,916,201,202,203,224,200,204),
   additional_fields = c("Site_Name", "Network_Name", "Phenophase_Category", "Observed_Status_Conflict_Flag"),
   climate_data = TRUE
 )
 
-write.csv(df, file="EPurpConeflower_2009-2023.csv")
+write.csv(df2022, file="8priority_spp_2022.csv")
+df2022 <- (read.csv("8priority_spp_2022.csv"))
 
-df <- (read.csv("EPurpConeflower_2009-2023.csv"))
+#create the list of stations needed to run daymet_download.R
+stations <- df2022 %>% distinct(site_id, latitude, longitude)
 
-#subset to flowering data (flowers/flower buds and open flowers)
+#pre 2022 other years
+df <- npn_download_individual_phenometrics(
+  request_source = 'Alyssa', 
+  years = c(2009:2021), 
+  species_ids = c(931,916,201,202,203,224,200,204),
+  additional_fields = c("Site_Name", "Network_Name", "Phenophase_Category", "Observed_Status_Conflict_Flag"),
+  climate_data = TRUE
+)
 
-df = df %>% 
-  subset(phenophase_category == 'Flowers') %>% 
+write.csv(df, file="8priority_spp_2009-2021.csv")
+df <- (read.csv("8priority_spp_2009-2021.csv"))
 
+#load daymet data by station reference file (made using daymet_download.R)
+daymet2022 <- (read.csv("daymet_ref_file_2022.csv"))
 
-#not used:
-#subsetting to the region yields only 3 records and to lower lats 35(?) 
-subset(state == c("TX", "LA", "OK", "NM")) 
-subset(latitude < 37)
-#subsetting to only first yes in the year didn't impact model fit/change outliers, so leaving all first yeses
-group_by(first_yes_year, individual_id, phenophase_description) %>%
-filter(first_yes_doy == min(first_yes_doy))
+#merge daymet ref file back with 2022 observational data
+df2022_daymet <- merge(df2022, daymet2022, by = c("site_id"), all=TRUE)
+colnames(df2022_daymet)
+
+#remove unneeded columns (note - these are for the version from excel with the extra index added)
+df2022_daymet <- select(df2022_daymet, -c(2,30:51))
+#remove the .y from the merged daymet file col names
+colnames(df2022_daymet) <- sub("\\.y","",colnames(df2022_daymet))
+
+#helper for figuring out mismatch between columns in two files
+cols_intersection <- intersect(df2022_daymet, df)
+
+#remove unneeded cols in df (gdd, acc_prcp, daylength, that we aren't using, to facilitate the bind)
+df <- select(df, -c(1,30,31,36,37,42,43,48:50))
+
+#row bind, FINALLY make a dataset with all the priority species data and daymet data for 2022
+df_complete <- rbind(df2022_daymet, df)
+
+write.csv(df_complete, file="8priority_spp_2009-2022.csv")
+
+df_complete <- (read.csv("8priority_spp_2009-2022.csv"))
+
+EPC = df_complete %>% subset(phenophase_category == 'Flowers' & species_id == 202) 
+
+#Calls that can be used to see how much data in the project region/lower lats:
+#subset(state == c("TX", "LA", "OK", "NM")) 
+#subset(latitude < 37)
+
+#subsetting to only first yes in the year - didn't impact model fit/change outliers, so leaving all first yeses
+#group_by(first_yes_year, individual_id, phenophase_description) %>%
+#filter(first_yes_doy == min(first_yes_doy))
+
+#viz amt of data by year
+hist(EPC$first_yes_year)
 
 #set site id to factor so it can be used as a random factor
-df$site_id_factor <- as.factor(df$site_id)
+EPC$site_id_factor <- as.factor(EPC$site_id)
 
 #create five lat bands named by major mid western cities for looking at temp sensitivity by lat
-df$lat_bans <- as.factor(cut(df$latitude, c(-Inf,30,35,40,45,Inf), c("Orlando", "Jackson", "OKCity", "Madison", "Bismark")))
-
-#remedy NAs in climate data (not using gdd or acc_prcp bc correlated w DOY)
-df <- df %>% 
-  mutate(tmax_spring = as.numeric(tmax_spring)) %>%
-  mutate(tmax_fall = as.numeric(tmax_fall)) %>%
-  mutate(tmax_summer = as.numeric(tmax_summer)) %>%
-  mutate(tmax_winter = as.numeric(tmax_winter)) %>%
-  mutate(tmin_spring = as.numeric(tmin_spring)) %>%
-  mutate(tmin_fall = as.numeric(tmin_fall)) %>%
-  mutate(tmin_summer = as.numeric(tmin_summer)) %>%
-  mutate(tmin_winter = as.numeric(tmin_winter)) %>%
-  mutate(prcp_spring = as.numeric(prcp_spring)) %>%
-  mutate(prcp_summer = as.numeric(prcp_summer)) %>%
-  mutate(prcp_winter = as.numeric(prcp_winter)) %>%
-  mutate(prcp_fall = as.numeric(prcp_fall)) 
-
-df <- df %>% 
-  mutate(tmax_spring = na_if(tmax_spring, -9999)) %>%
-  mutate(tmax_fall = na_if(tmax_fall, -9999)) %>%
-  mutate(tmax_winter = na_if(tmax_winter, -9999))%>%
-  mutate(tmax_summer = na_if(tmax_summer, -9999))%>%
-  mutate(tmin_spring = na_if(tmin_spring, -9999)) %>%
-  mutate(tmin_fall = na_if(tmin_fall, -9999)) %>%
-  mutate(tmin_winter = na_if(tmin_winter, -9999))%>%
-  mutate(tmin_summer = na_if(tmin_summer, -9999))%>%
-  mutate(prcp_spring = na_if(prcp_spring, -9999)) %>%
-  mutate(prcp_summer = na_if(prcp_summer, -9999)) %>%
-  mutate(prcp_winter = na_if(prcp_winter, -9999)) %>%
-  mutate(prcp_fall = na_if(prcp_fall, -9999))  
+EPC$lat_bans <- as.factor(cut(EPC$latitude, c(-Inf,30,35,40,45,Inf), c("Orlando", "Jackson", "OKCity", "Madison", "Bismark")))
   
 #explore records with observer conflicts
-conflict_summary <- df %>%
+conflict_summary <- EPC %>%
   count(phenophase_category, observed_status_conflict_flag) %>%
   group_by(phenophase_category) %>%
   mutate(observed_status_conflict_flag=recode(
     observed_status_conflict_flag,'MultiObserver-StatusConflict'='Multi', 'OneObserver-StatusConflict'='One')) %>%
   mutate(Percent_Conflict = n / sum(n))
 
-#decide not to remove any conflicting records, because represent less than 1% of records, thus Yes will override a No
+#decide not to remove any conflicting records, because represent less than 4% of records, thus Yes will override a No
 
-#limit to open flowers, and drop NAs
-OpenFlowers  <- df %>%
-  subset(phenophase_description == c('Open flowers')) %>%
-  subset(!is.na(tmax_spring)) 
+#limit to open flowers
+OpenFlowers  <- EPC %>% subset(phenophase_description == c('Open flowers')) 
 
-OpenFlowers_NoOutlier <- OpenFlowers %>%
-  subset(tmin_spring < 15)
-
-#only 14 records
-OpenFlowers_Jackson <- OpenFlowers %>%
-  subset(lat_bans == c("Jackson"))
-
-OpenFlowers_OKCity <- OpenFlowers %>%
-  subset(lat_bans == c("OKCity"))
+# dropping very warm spring outlier, didn't effect the model, didn't keep
+#OpenFlowers_NoOutlier <- OpenFlowers %>% subset(tmin_spring < 15)
 
 #determine how many site-years there are - this is only dif from # records where there are multiple onsets/year
 onsets_summary <- OpenFlowers %>%
@@ -111,25 +112,25 @@ onsets_summary <- OpenFlowers %>%
 sum(onsets_summary$years)
 
 #look at distribution of data
-hist(OpenFlowers$first_yes_doy)
 
-hist(OpenFlowers$tmax_spring)
-hist(OpenFlowers$tmax_fall)
-hist(OpenFlowers$tmax_summer)
-hist(OpenFlowers$tmax_winter)
-
-hist(OpenFlowers$tmin_spring)
-hist(OpenFlowers$tmin_fall)
-hist(OpenFlowers$tmin_summer)
-hist(OpenFlowers$tmin_winter)
-
-hist(OpenFlowers$prcp_spring)
-hist(OpenFlowers$prcp_fall)
-hist(OpenFlowers$prcp_summer)
-hist(OpenFlowers$prcp_winter)
-
-hist(OpenFlowers$latitude)
-
+par(mfrow = c(1, 2))
+hist(OpenFlowers$first_yes_doy, main = "Day of Year")
+hist(OpenFlowers$latitude, main = "Latitude")
+par(mfrow = c(2, 2))
+hist(OpenFlowers$tmax_spring, main = "Spring Tmax")
+hist(OpenFlowers$tmax_fall, main = "Fall Tmax")
+hist(OpenFlowers$tmax_summer, main = "Summer Tmax")
+hist(OpenFlowers$tmax_winter, main = "Winter Tmax")
+par(mfrow = c(2, 2))
+hist(OpenFlowers$tmin_spring, main = "Spring Tmin")
+hist(OpenFlowers$tmin_fall, main = "Fall Tmin")
+hist(OpenFlowers$tmin_summer, main = "Summer Tmin")
+hist(OpenFlowers$tmin_winter, main = "Winter Tmin")
+par(mfrow = c(2, 2))
+hist(OpenFlowers$prcp_spring, main = "Spring Precip")
+hist(OpenFlowers$prcp_fall, main = "Fall Precip")
+hist(OpenFlowers$prcp_summer, main = "Summer Precip")
+hist(OpenFlowers$prcp_winter, main = "Winter Precip")
 
 #Simple Linear Regression
 #plot a linear model of first day that "open flowers" were observed against climate variables
@@ -137,62 +138,62 @@ hist(OpenFlowers$latitude)
 ggplot(data = OpenFlowers, aes(x = tmin_spring, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1) 
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmax_spring, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmax_winter, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmin_winter, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmax_fall, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmin_fall, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmax_summer, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = tmin_summer, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = prcp_winter, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = prcp_spring, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = prcp_fall, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 ggplot(data = OpenFlowers, aes(x = prcp_summer, y = first_yes_doy)) +
   stat_cor() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1)
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1)
 
 #relevant predictors - tmin_spring, tmax_spring, tmax_winter, tmin_winter, tmax_summer, tmin_summer, tmax_fall, tmin_fall, prcp_fall,prcp_summer
 
@@ -234,29 +235,32 @@ abline(siegel_model, col="blue",lwd=2)
 #https://webinar-free-download.s3.amazonaws.com/COSA-Fixed_and_Random_Factors_in_Mixed_Models-Slides-Handout.pdf
 # and https://mspeekenbrink.github.io/sdam-r-companion/linear-mixed-effects-models.html
 
-#look at how variables range at sites
+#look at how variables range at sites - helps to decide if site should indeed be a random effect
 boxplot(first_yes_doy ~ site_id, data = OpenFlowers)
 boxplot(latitude ~ site_id, data = OpenFlowers)
 boxplot(tmin_spring ~ site_id, data = OpenFlowers)
 boxplot(first_yes_doy ~ state, data = OpenFlowers)
 
 
-#model 1
+#model 1, first yes predicted by tmin_spring, with site as a random effect
 lmer1 <- lmer(first_yes_doy~tmin_spring + (1| site_id_factor), data = OpenFlowers)
 summary(lmer1)
 
-#REML criterion at convergence: 1432.4
-#proportion of variance from sites ~1/3
+#REML criterion at convergence: 2040 
+#(compare these arcoss models - looking for lowest)
+#proportion of variance from sites ~1/3 
+#(you get this from the Random Effects section of the model summary - Random Effects - the site_id variance vs the site_id + residual variance
+#(ie, of all the variance how much is attributable to site)
 
 #Q-Q plots to check residuals
 qqnorm(resid(lmer1))
 qqline(resid(lmer1))  
 
 #model 2
-lmer2 <- lmer(first_yes_doy~tmin_spring + prcp_summer + (1| site_id_factor), data = OpenFlowers)
+lmer2 <- lmer(first_yes_doy~tmin_spring + prcp_winter + (1| site_id_factor), data = OpenFlowers)
 summary(lmer2)
 
-#REML criterion at convergence: 1437.31763.2
+#REML criterion at convergence: 2045
 #proportion of variance from sites ~1/3
 
 #Q-Q plots to check residuals
@@ -267,13 +271,13 @@ qqline(resid(lmer2))
 #make the more complex model the first argument
 anova(lmer2, lmer1)
 
-#adding summer prcp does not improve the model (P ChiSq 0.36)
+#adding winter prcp does not improve the model (P ChiSq 0.74)
 
 #model 3
-lmer3 <- lmer(first_yes_doy~tmin_spring + tmin_summer + (1| site_id_factor), data = OpenFlowers)
+lmer3 <- lmer(first_yes_doy~tmin_spring + tmin_winter + (1| site_id_factor), data = OpenFlowers)
 summary(lmer3)
 
-#REML criterion at convergence: 1427.8
+#REML criterion at convergence: 2038
 #proportion of variance from sites ~1/3
 
 #Q-Q plots to check residuals
@@ -282,13 +286,13 @@ qqline(resid(lmer3))
 
 anova(lmer3, lmer1)
 
-#adding summer temp also does not improve, P chisq 0.36
+#adding winter temp also does not improve, P chisq 0.53
 
 #model 4
 lmer4 <- lmer(first_yes_doy~tmin_spring*latitude + (1| site_id_factor), data = OpenFlowers)
 summary(lmer4)
 
-#REML criterion at convergence: 1415.7
+#REML criterion at convergence: 2031
 #proportion of variance from sites ~1/3
 
 #Q-Q plots to check residuals
@@ -297,52 +301,31 @@ qqline(resid(lmer4))
 
 anova(lmer4, lmer1)
 
-#interaction of spring temp and latitude does improve model - P chiSq 0.0005
+#interaction of spring temp and latitude does improve model - P chiSq 0.02
+#19 day delay is very unlikely
 
-
-#model 5
+#model 5 - is using lat_bands defensible?)
 lmer5 <- lmer(first_yes_doy~tmin_spring*lat_bans + (1| site_id_factor), data = OpenFlowers)
 summary(lmer5)
 
-#REML criterion at convergence: 1749.9
+#REML criterion at convergence: 1984 (lowest of this set of 5)
 #proportion of variance from sites ~1/5
 
 #Q-Q plots to check residuals
 qqnorm(resid(lmer5))
 qqline(resid(lmer5))
 
-anova(lmer5, lmer1)
+anova(lmer5, lmer4)
 
-#interaction of spring temp and latitude bands does improve model - P chiSq 0.000004
+#interaction of spring temp and latitude bands doesn't improve model, tho not far - P chiSq 0.066
+#effect size of 9 delay very unlikely
 
-#what about this very warm spring 
-
-
-
-#visually explore this model
+#visually explore model 4
 p <- ggplot(OpenFlowers, aes(x=tmin_spring, y=first_yes_doy, color=lat_bans)) +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1) 
-p  
-
-#seems to be driven by low latitude very warm spring, what if remove the outlier in the Jackson bin where tmin_spring >15
-
-p <- ggplot(OpenFlowers_NoOutlier, aes(x=tmin_spring, y=first_yes_doy, color=lat_bans)) +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1) 
+  stat_smooth(method = "lm", formula = y~x , linewidth = 1) 
 p 
 
-#Ok, so to interpret this, am I looking for the model with the lowest criterion? Which is model 1
 
 #And, then maybe it would be easier to get the effects, like spring min temps advance/delay by X days, the interaction
 #effect is like: after accounting for latitude, spring temps delay bloom by X days?
-
-ggplot(data = OpenFlowers_Jackson, aes(x = tmin_spring, y = first_yes_doy)) +
-  stat_cor() +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1) 
-
-ggplot(data = OpenFlowers_OKCity, aes(x = tmin_spring, y = first_yes_doy)) +
-  stat_cor() +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~x , size = 1) 
---
+++ daymet_download.R
@@ -0,0 +1,119 @@
+library(daymetr)
+library(lubridate)
+library(dplyr)
+rm(list=ls())
+
+DataFolder <- "~/Documents/My Files/USA-NPN/Data/Analysis/R_default/npn_analyses/TimetoRestore/Data"
+
+#https://tmieno2.github.io/R-as-GIS-for-Economists/daymet-with-daymetr-and-feddata.html
+
+get_daymet <- function(i){
+  
+  temp_lat <- stations[i, ] %>% pull(latitude)
+  temp_lon <- stations[i, ] %>% pull(longitude)
+  temp_site <-stations[i, ] %>% pull(site_id)
+  
+  temp_daymet <- download_daymet(
+    lat = temp_lat,
+    lon = temp_lon,
+    start = 2021,
+    end = 2022
+  ) %>% 
+    #--- just get the data part ---#
+    .$data %>% 
+    #--- convert to tibble (not strictly necessary) ---#
+    as_tibble() %>% 
+    #--- assign site_id so you know which record is for which site_id ---#
+    mutate(site_id = temp_site) %>% 
+    #--- get date from day of the year ---#
+    mutate(date = as.Date(paste(year, yday, sep = "-"), "%Y-%j"))
+  
+  return(temp_daymet)
+}  
+
+#check out the results of the first row
+get_daymet(1)
+
+#run for all stations
+(
+  daymet_df <- lapply(1:nrow(stations), get_daymet) %>% 
+    #--- need to combine the list of data.frames into a single data.frame ---#
+    bind_rows()
+)
+
+#https://carpentries-incubator.github.io/data-harvesting-for-agriculture/09-WeatherData/index.html
+daymet_df$month <- lubridate::month(daymet_df$date, label = TRUE)
+head(daymet_df$month)
+
+str(daymet_df)
+
+write.csv(daymet_df, file="daymet_stations.csv")
+                              
+fall_ref_df <- daymet_df %>%
+  group_by(site_id)  %>%
+  filter(year == 2021, month %in% c("Sep","Oct","Nov")) %>%
+  mutate(prcp_fall = (sum(prcp..mm.day.))) %>%
+  mutate(tmin_fall = (mean(tmin..deg.c.)))%>%
+  mutate(tmax_fall = (mean(tmax..deg.c.)))%>%
+  distinct(site_id, .keep_all = TRUE)
+
+
+winter_ref_df <- daymet_df %>%
+  group_by(site_id)  %>%
+  filter(year == 2021 & month %in% c("Dec") | year == 2022 & month %in% c("Jan","Feb")) %>%
+  mutate(prcp_winter = (sum(prcp..mm.day.))) %>%
+  mutate(tmin_winter = (mean(tmin..deg.c.)))%>%
+  mutate(tmax_winter = (mean(tmax..deg.c.)))%>%
+  distinct(site_id, .keep_all = TRUE)
+
+spring_ref_df <- daymet_df %>%
+  group_by(site_id)  %>%
+  filter(year == 2022, month %in% c("Mar","Apr","May")) %>%
+  mutate(prcp_spring = (sum(prcp..mm.day.))) %>%
+  mutate(tmin_spring = (mean(tmin..deg.c.)))%>%
+  mutate(tmax_spring = (mean(tmax..deg.c.)))%>%
+  distinct(site_id, .keep_all = TRUE)
+
+
+summer_ref_df <- daymet_df %>%
+  group_by(site_id)  %>%
+  filter(year == 2022, month %in% c("Jun","Jul","Aug")) %>%
+  mutate(prcp_summer = (sum(prcp..mm.day.))) %>%
+  mutate(tmin_summer = (mean(tmin..deg.c.)))%>%
+  mutate(tmax_summer = (mean(tmax..deg.c.)))%>%
+  distinct(site_id, .keep_all = TRUE)
+
+
+spring_fall <- merge(fall_ref_df, spring_ref_df, by = "site_id")
+
+sum_win <- merge(summer_ref_df, winter_ref_df, by = "site_id")
+
+all_ref <- merge(sum_win, spring_fall, by = "site_id")
+all_ref1 <- all_ref %>%
+  select("site_id","prcp_summer","prcp_fall","prcp_winter", "prcp_spring",
+         "tmin_summer","tmin_fall","tmin_winter","tmin_spring",
+         "tmax_summer","tmax_fall","tmax_winter","tmax_spring")
+
+
+hist(all_ref1$prcp_summer)
+hist(all_ref1$tmax_summer)
+hist(all_ref1$tmin_summer)
+
+write.csv(all_ref1, file="daymet_ref_file_2022.csv")
+
+setwd(DataFolder)
+daymet2022 <- (read.csv("daymet_ref_file_2022.csv"))
+
+#code to use if you just want one point
+download_daymet(
+  site = "Daymet",
+  lat = 36.0133,
+  lon = -84.2625,
+  start = 2021,
+  end = 2022,
+  internal = FALSE,
+  path = DataFolder,
+  silent = FALSE,
+  force = FALSE,
+  simplify = TRUE
+)
