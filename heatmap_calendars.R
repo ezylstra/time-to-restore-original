e4aaec9c4a2f47a5f71d42d2962fbd98e40872ac
travismatlock <122936606+travismatlock@users.noreply.github.com>
2023-05-30 13:37:17 -0700

    Community Calendars
    Uploaded file with code for differently colored species plots stacked vertically

--- heatmap_calendars.R
+++ heatmap_calendars.R
@@ -8,6 +8,7 @@ library(lubridate)
 library(ggplot2)
 library(patchwork)
 library(RColorBrewer)
+library(ggbreak)
 rm(list= ls())
 
 #Notes
@@ -27,14 +28,15 @@ species_list <- npn_species()
 #Find Time to Restore - priority species - https://docs.google.com/spreadsheets/d/1WyxsCqZAAATIgZvR5ewNcZkaN2mumVi2d1vFw8I0HJo/edit#gid=0
 
 
-species <- c(931,916,201,202,203,224,200,204,845,207,781,197,1334,1163,186,1167)
+species <- c(186,197,200,201,202,203,204,207,224,781,845,916,931,1163,1167,1334)
+phenophases = c(501, 390) # open flowers and ripe fruits
 
 df <- npn_download_status_data(request_source="Alyssa",
                                years=c(2009:2023),
                                species_ids = c(paste0(species)), # species codes
                                state = c("TX", "LA", "OK", "NM"),
                                additional_fields = c("observedby_person_id"),
-                               phenophase_ids= c(501), # open flowers
+                               phenophase_ids= c(paste0(phenophases)), 
                                climate_data = FALSE)
 
 
@@ -54,7 +56,6 @@ colnames(df)
 
 df$common_name <- as.factor(df$common_name)
 df$phenophase_status <- as.numeric(df$phenophase_status)
-df$phenophase_description <- as.factor(df$phenophase_description)
 
 #Ignore for now - filters used to get buttonbush observer variability plot
   filter(individual_id == 141920) %>%
@@ -63,7 +64,7 @@ df$phenophase_description <- as.factor(df$phenophase_description)
 
 #calculate the proportion of records in to the total records by week
 df1 <- df %>%
-  group_by(week, common_name) %>%
+  group_by(week, common_name, phenophase_id) %>%
   mutate(num_records = n()) %>%
   mutate(sum_pp = sum(phenophase_status))%>%
   mutate(proportion = sum_pp/num_records) %>%
@@ -87,6 +88,11 @@ df_peak <- df_peak %>%
 #ignore for now - hm, there are some NAs unexpected in both estimate of open flowers and in proportions - why?
 str(df_peak)
 
+# Creates dataframe for which species should use which colors - not used currently
+species_color <- data.frame(species_id = species, color=c("lightgoldenrod1", "palevioletred1", "palegreen1", "burlywood1",
+                                                          "plum1", "gold", "orchid2", "papayawhip", "rosybrown1", "indianred1",
+                                                          "purple1", "firebrick1", "thistle2", "darkolivegreen1", "yellow", "lightcyan2"))
+
 # Create data frames based on states' priority species
 df_LA <- df1 %>%
   # Filter based on states relevant to selected species for this state
@@ -94,28 +100,28 @@ df_LA <- df1 %>%
   # Filter for species
   filter(species_id == 201 | species_id == 1334) %>%
   # Assign different colors to different species
-  mutate(color = case_when(species_id == 201 ~ "green",
-                           species_id == 1334 ~ "pink"))
+  mutate(color = case_when(species_id == 201 ~ "burlywood1",
+                           species_id == 1334 ~ "lightcyan2"))
 # Repeat for all states
 df_OK <- df1 %>%
   filter(state != "NM" ) %>%
   filter(species_id == 781 | species_id == 201 | species_id == 203) %>%
-  mutate(color = case_when(species_id == 201 ~ "green",
-                           species_id == 781 ~ "red",
-                           species_id == 203 ~ "yellow"))
+  mutate(color = case_when(species_id == 201 ~ "burlywood1",
+                           species_id == 781 ~ "indianred1",
+                           species_id == 203 ~ "gold"))
 df_NM <- df1 %>%
   filter(state == "NM" | state == "TX") %>%
   filter(species_id == 224 | species_id == 1163 | species_id == 1167 | species_id == 203) %>%
-  mutate(color = case_when(species_id == 224 ~ "pink",
-                           species_id == 1163 ~ "green",
-                           species_id == 1167 ~ "purple",
-                           species_id == 203 ~ "yellow"))
+  mutate(color = case_when(species_id == 224 ~ "rosybrown1",
+                           species_id == 1163 ~ "darkolivegreen1",
+                           species_id == 1167 ~ "yellow",
+                           species_id == 203 ~ "gold"))
 df_TX <- df1 %>%
   filter(state == "TX") %>%
   filter(species_id == 201 | species_id == 202 | species_id == 203) %>%
-  mutate(color = case_when(species_id == 201 ~ "green",
-                           species_id == 202 ~ "purple",
-                           species_id == 203 ~ "yellow"))
+  mutate(color = case_when(species_id == 201 ~ "burlywood1",
+                           species_id == 202 ~ "plum1",
+                           species_id == 203 ~ "gold"))
 
 # Plotting for peak
 p = ggplot() +
@@ -131,39 +137,31 @@ plot(p)
 
 plots <- list()
 # Loop over each species
-for (i in unique(df_TX$common_name)) {
+for (i in unique(df_NM$common_name)) {
   # Create temporary dataset filtered by individual species
-  tmp <- df_TX %>%
-    filter(common_name == i)
+  tmp <- df_NM %>%
+    filter(common_name == i) 
   # Add the graphs for each individual species to the plot list
   plots[[i]] <- tmp %>%
     ggplot() +
     geom_tile(aes(x=week, y=phenophase_description, fill=proportion)) +
-    theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank(), legend.position="left") +
+    theme(axis.text.y = element_text(), axis.ticks.y = element_blank(), legend.position="none", panel.background = element_blank()) +
     labs(x = "", y ="") +
     scale_fill_gradient(low="grey", high=tmp$color[1], (name="Proportion")) +
-    # Uncomment following line for color palette, same for all species
-    # scale_fill_gradientn(colors=hcl.colors(35, "Emrld"), (name="Proportion")) +
-    facet_wrap(tmp$common_name, nrow=1, strip.position="top")
+    facet_wrap(tmp$common_name, strip.position="top") + 
+    scale_x_continuous(breaks=seq(1, 52, by=4.25), limits=c(1,52),
+                       labels=c("J","F","M", "A", "M", "J", "J", "A", "S", "O", "N", "D", "")) #+
+    #coord_fixed(ratio=2) # Can be used to adjust aspect ratio if needed
 }
+
 # Stack all plots
-purrr::reduce(plots, `+`)
+purrr::reduce(plots, `/`)
+# For a 2x2 grid
+(plots[[1]] + plots[[2]]) / (plots[[3]] + plots[[4]])
 
-# Code for desired plots where species are outlined in different colors, but gradient is uniform
-# Delete color input in geom_tile for smooth gradient (no outline)
-p <- ggplot(df_TX) +
-  geom_tile(aes(x = week, y = phenophase_description, color=common_name, fill = proportion)) +
-  # Code for potential coloration fix - not working
-  #scale_fill_manual(values = brewer.pal(name="Set1", n = 3), guide="none") +
-  #scale_alpha_manual(values=rep(c("red","purple","green", "blue", "yellow"), 7)) +
-  scale_fill_gradient(low = "grey", high = "purple", (name = "Proportion")) +
-  facet_wrap(df_TX$common_name, ncol=1, strip.position="top") +
-  labs(x = "", y ="") +
-  theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank(), legend.position="left") +
-  theme(axis.text = element_text(size = 13))
-plot(p)
 
-p2 = ggplot() +
+
+ p2 = ggplot() +
   geom_line(data = df_peak, aes(x=week, y=open_flower_estimate),color="orange", size = 2) +
   labs(y = "# Open Flowers ", x = " ") +
   theme(axis.text.x = element_blank(),axis.text.y = element_blank(),axis.ticks = element_blank())
